d921db52a1f1cc9d77aefac2669095b8a23ca3e7
kele00011 <3299075637@qq.com>
2025-10-28 20:51:06 +0800

    删除报错文件

--- alpha/Assets/VFX/QFramework-master/Framework/1.Core/ActionKit/Fluent/IDisposeWhen.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace QFramework
-{
-    public interface IDisposeWhen : IDisposeEventRegister
-    {
-        IDisposeEventRegister DisposeWhen(Func<bool> condition);
-    }
-}
